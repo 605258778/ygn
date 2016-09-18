e9a941cfe5db58f0157e7ae160f4dc69cfb7885d
605258778 <605258778@qq.com>
2016-09-18 20:27:49 +0800

    修改:模板文件

--- js/page.js
+++ js/page.js
@@ -17,7 +17,25 @@ $(function() {
         $(this).find("ins").stop().animate({ left: -290 }, 600);
     });
 
+    $("#menu1 li a").wrapInner( '<span class="out"></span>' ).append( '<span class="bg"></span>' );
 
+            // loop each anchor and add copy of text content
+            $("#menu1 li a").each(function() {
+                $( '<span class="over">' +  $(this).text() + '</span>' ).appendTo( this );
+            });
+
+            $("#menu1 li a").hover(function() {
+                // this function is fired when the mouse is moved over
+                $(".out",   this).stop().animate({'top':    '40px'},    250); // move down - hide
+                $(".over",  this).stop().animate({'top':    '0px'},     250); // move down - show
+                $(".bg",    this).stop().animate({'top':    '0px'},     120); // move down - show
+
+            }, function() {
+                // this function is fired when the mouse is moved off
+                $(".out",   this).stop().animate({'top':    '0px'},     250); // move up - show
+                $(".over",  this).stop().animate({'top':    '-40px'},   250); // move up - hide
+                $(".bg",    this).stop().animate({'top':    '-40px'},   120); // move up - hide
+            });
     $(".video ul li").hover(function() {
         $(this).find("ins").stop().animate({ top: 0 }, 600);
     }, function() {
